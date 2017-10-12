3e99f788e5b860d86a557a6e01c4b7b6e01076c9
Levy Costa <levycosta471@gmail.com>
2017-10-12 15:34:17 -0300

    leve correcao na parte principal

--- TrabalhoGrupoo/src/br/ufsc/ine5605/Trabalho01/TelaPrincipal.java
+++ TrabalhoGrupoo/src/br/ufsc/ine5605/Trabalho01/TelaPrincipal.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -37,43 +37,4 @@ public class TelaPrincipal {
         System.out.println("Opção inválida.");
     }
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package br.ufsc.ine5605.Trabalho01;
 
-/**
- *
- * @author carcaroff
- */
-import java.util.Scanner;
-
-public class TelaPrincipal {
-
-    private Scanner sc;
-    
-    public TelaPrincipal(){
-        sc = new Scanner(System.in);
-    }
-    public int exibeMenuPrincipal() {
-        System.out.println("----------Tela Principal--------");
-        System.out.println("Selecione a opção desejada: ");
-        System.out.println("1 - Menu de Acessos.");
-        System.out.println("2 - Menu de Cargos.");
-        System.out.println("3 - Menu de Funcionários.");
-        System.out.println("0 - Fechar o Sistema.");
-        return sc.nextInt();
-    }
-    
-    public void print(String mensagem){
-        System.out.println(mensagem);
-    }
-
-    public void printInvalidOptionError() {
-        System.out.println("Opção inválida.");
-    }
-}
->>>>>>> b235df9ceed4432e58d7b22c919e3c28c9b30d4c
